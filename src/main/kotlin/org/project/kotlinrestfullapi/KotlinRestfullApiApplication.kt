06cd57ad8bdc66ceb481e9d38853b9583e2ba4cb
MasKoding <masdidi00@gmail.com>
2021-09-15 17:19:31 +0700

    build spring boot project

---
+++ src/main/kotlin/org/project/kotlinrestfullapi/KotlinRestfullApiApplication.kt
@@ -0,0 +1,11 @@
+package org.project.kotlinrestfullapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestfullApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRestfullApiApplication>(*args)
+}
---
+++ src/test/kotlin/org/project/kotlinrestfullapi/KotlinRestfullApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.project.kotlinrestfullapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestfullApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
